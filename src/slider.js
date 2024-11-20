4c025ec32fc381ca8b6e21d992b53f32ecb05235
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-11-20 13:18:44 +0500

    make dots logic

--- src/slider.js
+++ src/slider.js
@@ -3,16 +3,20 @@ const rightBtn = document.querySelector(".right");
 const dots = document.querySelectorAll(".dot");
 const images = document.querySelectorAll(".images>img");
 
+fillDot(0);
+
 rightBtn.addEventListener("click", () => {
     for (let i = 0; i < images.length; i++) {
         if (images[i].classList.contains("visible")) {
             if (i == images.length - 1) {
                 images[i].classList.remove("visible");
                 images[0].classList.add("visible");
+                fillDot(0);
                 break;
             } else {
                 images[i].classList.remove("visible");
                 images[i + 1].classList.add("visible");
+                fillDot(i + 1);
                 break;
             }
         }
@@ -25,12 +29,21 @@ leftBtn.addEventListener("click", () => {
             if (i == 0) {
                 images[i].classList.remove("visible");
                 images[images.length - 1].classList.add("visible");
+                fillDot(images.length - 1);
                 break;
             } else {
                 images[i].classList.remove("visible");
                 images[i - 1].classList.add("visible");
+                fillDot(i - 1);
                 break;
             }
         }
     }
 });
+
+function fillDot(index) {
+    dots.forEach((dot) => {
+        dot.style.color = "grey";
+    });
+    dots[index].style.color = "#c0c0c0";
+}
