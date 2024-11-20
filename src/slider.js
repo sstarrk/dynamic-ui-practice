94d7d2698f31a1c0e9ce94599c484fc38c2abd9f
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-11-20 13:34:24 +0500

    make every 5 second show next image

--- src/slider.js
+++ src/slider.js
@@ -5,41 +5,50 @@ const images = document.querySelectorAll(".images>img");
 
 fillDot(0);
 
-rightBtn.addEventListener("click", () => {
-    for (let i = 0; i < images.length; i++) {
-        if (images[i].classList.contains("visible")) {
-            if (i == images.length - 1) {
-                images[i].classList.remove("visible");
-                images[0].classList.add("visible");
-                fillDot(0);
-                break;
-            } else {
-                images[i].classList.remove("visible");
-                images[i + 1].classList.add("visible");
-                fillDot(i + 1);
-                break;
-            }
-        }
-    }
-});
+let toRight;
+let toLeft;
 
-leftBtn.addEventListener("click", () => {
-    for (let i = 0; i < images.length; i++) {
-        if (images[i].classList.contains("visible")) {
-            if (i == 0) {
-                images[i].classList.remove("visible");
-                images[images.length - 1].classList.add("visible");
-                fillDot(images.length - 1);
-                break;
-            } else {
-                images[i].classList.remove("visible");
-                images[i - 1].classList.add("visible");
-                fillDot(i - 1);
-                break;
+rightBtn.addEventListener(
+    "click",
+    (toRight = () => {
+        for (let i = 0; i < images.length; i++) {
+            if (images[i].classList.contains("visible")) {
+                if (i == images.length - 1) {
+                    images[i].classList.remove("visible");
+                    images[0].classList.add("visible");
+                    fillDot(0);
+                    break;
+                } else {
+                    images[i].classList.remove("visible");
+                    images[i + 1].classList.add("visible");
+                    fillDot(i + 1);
+                    break;
+                }
             }
         }
-    }
-});
+    })
+);
+
+leftBtn.addEventListener(
+    "click",
+    (toLeft = () => {
+        for (let i = 0; i < images.length; i++) {
+            if (images[i].classList.contains("visible")) {
+                if (i == 0) {
+                    images[i].classList.remove("visible");
+                    images[images.length - 1].classList.add("visible");
+                    fillDot(images.length - 1);
+                    break;
+                } else {
+                    images[i].classList.remove("visible");
+                    images[i - 1].classList.add("visible");
+                    fillDot(i - 1);
+                    break;
+                }
+            }
+        }
+    })
+);
 
 function fillDot(index) {
     dots.forEach((dot) => {
@@ -57,3 +66,5 @@ dots.forEach((dot, index) => {
         fillDot(index);
     });
 });
+
+setInterval(toRight, 5000);
