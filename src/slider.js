983582612f53523265b32c3799bed75bf7f6cbbb
sstarrk <170336793+sstarrk@users.noreply.github.com>
2024-11-20 12:52:26 +0500

    make button right logic

--- src/slider.js
+++ src/slider.js
@@ -1,4 +1,20 @@
 const leftBtn = document.querySelector(".left");
 const rightBtn = document.querySelector(".right");
 const dots = document.querySelectorAll(".dot");
-const images = document.querySelectorAll(".image");
+const images = document.querySelectorAll(".images>img");
+
+rightBtn.addEventListener("click", () => {
+    for (let i = 0; i < images.length; i++) {
+        if (images[i].classList.contains("visible")) {
+            if (i == images.length - 1) {
+                images[i].classList.remove("visible");
+                images[0].classList.add("visible");
+                break;
+            } else {
+                images[i].classList.remove("visible");
+                images[i + 1].classList.add("visible");
+                break;
+            }
+        }
+    }
+});
